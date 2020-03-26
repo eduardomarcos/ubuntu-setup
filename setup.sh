fa003c056ea82bc9320d2934d8cab0febe892e19
eduardomarcos <dudy1984@gmail.com>
2020-03-26 19:42:12 +0100

    init setup with zsh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install --yes curl 
+sudo apt-get install --yes vim
+sudo apt-get install --yes htop
+sudo apt-get install --yes xclip
+sudo apt-get install --yes ranger
+
+#Oh-My-Zsh
+sudo apt-get install --yes zsh
+sudo apt-get install --yes fonts-powerline
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Copying exported zsh configuration..."
+cp ./zsh/.zshrc /home/$USER/
