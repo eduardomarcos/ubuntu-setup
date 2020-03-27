4673523fc43191296bf2999e69f20eabbd30ee28
eduardomarcos <dudy1984@gmail.com>
2020-03-27 19:37:28 +0100

    removed unused extensions

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -4,16 +4,8 @@
 sudo snap install code --classic
 
 # Extensions
-code --install-extension christian-kohler.npm-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension eamodio.gitlens
-code --install-extension isudox.vscode-jetbrains-keybindings
-code --install-extension keesschollaart.vscode-home-assistant
-code --install-extension mikestead.dotenv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode.cpptools
 code --install-extension platformio.platformio-ide
-code --install-extension redhat.vscode-yaml
 code --install-extension sdras.night-owl
-code --install-extension xabikos.JavaScriptSnippets
