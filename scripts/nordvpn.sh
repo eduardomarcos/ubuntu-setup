cf1e7de12a0a5ef2cccb61c0cc27f651008108e5
eduardomarcos <dudy1984@gmail.com>
2020-03-27 19:52:19 +0100

    fix relative path for nordvpn package

--- scripts/nordvpn.sh
+++ scripts/nordvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # NordVpn
-sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb | sudo dpkg -i nordvpn-release_1.0.0_all.deb
+sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb | sudo dpkg -i ./nordvpn-release_1.0.0_all.deb
 sudo apt-get update &&  sudo apt-get install --yes nordvpn
 sudo rm nordvpn-release_1.0.0_all.deb
