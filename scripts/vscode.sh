1a7e5afe71bc0e5ddd03e2dfcb4f540b0020eb20
eduardomarcos <dudy1984@gmail.com>
2020-03-27 19:46:08 +0100

    back to apt-get repository to install vscode
    code snippet is installed by using this method, no with snap

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Visual Studio Code
-sudo snap install code --classic
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install --yes apt-transport-https
+sudo apt-get update && sudo apt-get install --yes code
 
 # Extensions
 code --install-extension CoenraadS.bracket-pair-colorizer
